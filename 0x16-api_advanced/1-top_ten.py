363b36d5244a21b6a7e4911347cf572e04b4eaa5
Randy0607 <randall.julies89@gmail.com>
2024-03-07 16:25:42 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+Python script
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
-
+    """function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    params = {'limit': 10}
+    headers = {'User-agent': 'BotHeiny'}
+    try:
+        response = requests.get(url,
+                                headers=headers,
+                                params=params,
+                                allow_redirects=False).json()
+        data = response['data']['children']
+        for listing in data:
+            title = listing.get('data').get('title')
+            print(title)
+    except:
+        print('None')
